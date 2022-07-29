07da03353ffbd3d4b1920126abe29f71010c4fc9
PRIYANKA-Nigam <nigampriyanka042@gmail.com>
2022-07-29 14:30:52 +0530

    6th commit

--- app/src/main/java/com/example/quotesandjokes/InternationalEventsFavActivity.java
+++ app/src/main/java/com/example/quotesandjokes/InternationalEventsFavActivity.java
@@ -10,10 +10,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -25,12 +27,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class InternationalEventsFavActivity extends AppCompatActivity {
     String fileName="",filePath="",fileContent ="";
     ListView listView; ArrayList<String> arrayList =new ArrayList<>();
-    ArrayAdapter adapter;
+    ArrayAdapter adapter;   ArrayList<String> userSelection =new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +47,8 @@ public class InternationalEventsFavActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         listView = findViewById(R.id.ll);
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(modeListener);
         adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -55,40 +60,14 @@ public class InternationalEventsFavActivity extends AppCompatActivity {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 String s = arrayList.get(position);
-                                Intent sendIntent = new Intent();
-                                sendIntent.setAction(Intent.ACTION_SEND);
-                                sendIntent.putExtra(Intent.EXTRA_TEXT, "Good Wish : " + s);
-                                sendIntent.setType("text/plain");
-                                Intent shareIntent = Intent.createChooser(sendIntent, null);
-                                startActivity(shareIntent);
-                                Toast.makeText(getApplicationContext(), "Sharing Wish ...", Toast.LENGTH_SHORT).show();
-                                finish();
+                                sharedLogic(s);
                             }
                         }).setNeutralButton("Save", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        if (!isExternalStorageAvailableForRW()) {
-                            Toast.makeText(getApplicationContext(), "Sorry U don't have sdcard mounted on your device", Toast.LENGTH_SHORT).show();
-                        } else {
-                            String s = arrayList.get(position);
-                            fileContent = s;
-                            if (!fileContent.equals("")) {
-                                File file = new File(getExternalFilesDir(filePath), fileName);
-                                FileOutputStream fileOutputStream = null;
-                                try {
-                                    fileOutputStream = new FileOutputStream(file);
-                                    fileOutputStream.write(fileContent.getBytes());
-                                } catch (FileNotFoundException fileNotFoundException) {
-                                    fileNotFoundException.printStackTrace();
-                                } catch (IOException ioException) {
-                                    ioException.printStackTrace();
-                                }
-                                Toast.makeText(InternationalEventsFavActivity.this, "Wish Saved to SD Card", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(InternationalEventsFavActivity.this, "Text Field can not be Empty", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-
+                        String s = arrayList.get(position);
+                        fileContent = s;
+                       savedLogic(fileContent);
                     }
                 });
                 AlertDialog alert = a.create();
@@ -96,28 +75,81 @@ public class InternationalEventsFavActivity extends AppCompatActivity {
                 alert.show();
             }
         });
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                new AlertDialog.Builder(InternationalEventsFavActivity.this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("Are You Sure?")
-                        .setMessage("Do You Want to delete this Wish").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        arrayList.remove(position);
-                        adapter.notifyDataSetChanged();
-                        SharedPreferences sh = getApplicationContext().getSharedPreferences("International", Context.MODE_PRIVATE);
-                        HashSet<String> set = new HashSet<>(arrayList);
-                        sh.edit().putStringSet("event", set).apply();
-                        Toast.makeText(getApplicationContext(), "Deleted this Wish ,Now please Save again ...", Toast.LENGTH_SHORT).show();
-                    }
-                }).setNegativeButton("No", null).show();
-                return true;
-            }
-        });
+//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+//                new AlertDialog.Builder(InternationalEventsFavActivity.this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("Are You Sure?")
+//                        .setMessage("Do You Want to delete this Wish").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        arrayList.remove(position);
+//                        adapter.notifyDataSetChanged();
+//                        SharedPreferences sh = getApplicationContext().getSharedPreferences("International", Context.MODE_PRIVATE);
+//                        HashSet<String> set = new HashSet<>(arrayList);
+//                        sh.edit().putStringSet("event", set).apply();
+//                        Toast.makeText(getApplicationContext(), "Deleted this Wish ,Now please Save again ...", Toast.LENGTH_SHORT).show();
+//                    }
+//                }).setNegativeButton("No", null).show();
+//                return true;
+//            }
+//        });
         try {
             loadData();
         } catch (NullPointerException e) {
             e.printStackTrace();
+        }
+    }
+    public void removeItem(List<String> list){
+        for (String i : list){
+            arrayList.remove(i);
+        }
+    }
+    public void saveItem(List<String> list){
+        for (String s : list) {
+            fileContent += s+"\n";
+        }
+        savedLogic(fileContent);
+
+    }
+    public void shareItem(List<String> list){
+        String select="";
+        for (String s : list) {
+            //  String s = arrayList.get(i);
+            select+=s+"\n";
+        }
+        sharedLogic(select);
+
+    }
+    public void sharedLogic(String s){
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Interesting Quote : " + s);
+        sendIntent.setType("text/plain");
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
+        Toast.makeText(getApplicationContext(), "Sharing Quote ...", Toast.LENGTH_SHORT).show();
+        finish();
+
+    }
+    public void savedLogic(String s){
+        if (!isExternalStorageAvailableForRW()) {
+            Toast.makeText(getApplicationContext(), "Sorry U don't have sdcard mounted on your device", Toast.LENGTH_SHORT).show();
+        } else {
+            if (!fileContent.equals("")) {
+                File file = new File(getExternalFilesDir(filePath), fileName);
+                FileOutputStream fileOutputStream = null;
+                try {
+                    fileOutputStream = new FileOutputStream(file);
+                    fileOutputStream.write(fileContent.getBytes());
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                Toast.makeText(InternationalEventsFavActivity.this, "Quote Saved to SD Card", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(InternationalEventsFavActivity.this, "Text Field can not be Empty", Toast.LENGTH_SHORT).show();
+            }
         }
     }
     private void loadData() {
@@ -151,4 +183,55 @@ public class InternationalEventsFavActivity extends AppCompatActivity {
     }
     private boolean isExternalStorageAvailableForRW() { String extState= Environment.getExternalStorageState();
         if (extState.equals(Environment.MEDIA_MOUNTED)){ return true; } return false; }
+
+    AbsListView.MultiChoiceModeListener modeListener =new AbsListView.MultiChoiceModeListener() {
+        @Override
+        public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
+            if (userSelection.contains(arrayList.get(i))){
+                userSelection.remove(arrayList.get(i));
+            }else {
+                userSelection.add(arrayList.get(i));
+            }
+            actionMode.setTitle(userSelection.size() +" items selected...");
+        }
+
+        @Override
+        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+            MenuInflater menuInflater =actionMode.getMenuInflater();
+            menuInflater.inflate(R.menu.delete_save_share,menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.delete:
+                    removeItem(userSelection);
+                    adapter.notifyDataSetChanged();
+                    SharedPreferences sh = getApplicationContext().getSharedPreferences("International", Context.MODE_PRIVATE);
+                    HashSet<String> set = new HashSet<>(arrayList);
+                    sh.edit().putStringSet("event", set).apply();
+                    Toast.makeText(getApplicationContext(), "Deleted this Quote ...", Toast.LENGTH_SHORT).show();
+                    actionMode.finish();
+                    return true;
+                case R.id.share:
+                    shareItem(userSelection);
+                    return true;
+                case R.id.save:
+                    saveItem(userSelection);
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode actionMode) {
+            userSelection.clear();
+        }
+    };
 }
--- app/src/main/java/com/example/quotesandjokes/JokesFavActivity.java
+++ app/src/main/java/com/example/quotesandjokes/JokesFavActivity.java
@@ -12,10 +12,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -27,11 +29,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class JokesFavActivity extends AppCompatActivity { String fileName="",filePath="",fileContent ="";
     ListView listView; ArrayList<String> arrayList =new ArrayList<>();
-    ArrayAdapter adapter;
+    ArrayAdapter adapter;   ArrayList<String> userSelection =new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +46,8 @@ public class JokesFavActivity extends AppCompatActivity { String fileName="",fil
             e.printStackTrace();
         }
         listView=findViewById(R.id.ll);
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(modeListener);
         adapter=new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,arrayList);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -53,57 +58,92 @@ public class JokesFavActivity extends AppCompatActivity { String fileName="",fil
                         .setPositiveButton("Share",new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {  String s = arrayList.get(position);
-                                Intent sendIntent = new Intent();
-                                sendIntent.setAction(Intent.ACTION_SEND);
-                                sendIntent.putExtra(Intent.EXTRA_TEXT, "Interesting Joke : "+s);
-                                sendIntent.setType("text/plain");
-                                Intent shareIntent = Intent.createChooser(sendIntent, null);
-                                startActivity(shareIntent);
-                                Toast.makeText(getApplicationContext(), "Sharing Joke ...", Toast.LENGTH_SHORT).show();
-                                finish();  }}).setNeutralButton("Save", new DialogInterface.OnClickListener() {
+                               sharedLogic(s);
+                            }}).setNeutralButton("Save", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        if (!isExternalStorageAvailableForRW()) {
-                            Toast.makeText(getApplicationContext(),"Sorry U don't have sdcard mounted on your device",Toast.LENGTH_SHORT).show();
-                        }else {
-                            String s = arrayList.get(position);
-                            fileContent =s;
-                            if (!fileContent.equals("")){ File file=new File(getExternalFilesDir(filePath),fileName);
-                                FileOutputStream fileOutputStream=null;
-                                try { fileOutputStream=new FileOutputStream(file); fileOutputStream.write(fileContent.getBytes());
-                                } catch (FileNotFoundException fileNotFoundException) { fileNotFoundException.printStackTrace();
-                                } catch (IOException ioException) { ioException.printStackTrace(); }
-                                Toast.makeText(JokesFavActivity.this,"Joke Saved to SD Card",Toast.LENGTH_SHORT).show();
-                            }else {
-                                Toast.makeText(JokesFavActivity.this,"Text Field can not be Empty",Toast.LENGTH_SHORT).show(); }
-                        }
+                        String s = arrayList.get(position);
+                        fileContent =s;
+                       savedLogic(fileContent);
                     }
                 });
                 AlertDialog alert= a.create();alert.setTitle("Save or Share this Item");alert.show();
             }
         });
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                new AlertDialog.Builder(JokesFavActivity.this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("Are You Sure?")
-                        .setMessage("Do You Want to delete this Joke").setPositiveButton("Yes", new DialogInterface.OnClickListener() {@Override
-                public void onClick(DialogInterface dialog, int which) {
-                    arrayList.remove(position);adapter.notifyDataSetChanged();
-                    SharedPreferences sh=getApplicationContext().getSharedPreferences("safe", Context.MODE_PRIVATE);
-                    HashSet<String> set=new HashSet<>(arrayList);sh.edit().putStringSet("notes",set).apply();
-                    Toast.makeText(getApplicationContext(), "Deleted this Joke ,Now please Save again ...", Toast.LENGTH_SHORT).show();
-                }
-                }).setNegativeButton("No",null).show();
-                return true;
-            }
-        });
+//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+//                new AlertDialog.Builder(JokesFavActivity.this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("Are You Sure?")
+//                        .setMessage("Do You Want to delete this Joke").setPositiveButton("Yes", new DialogInterface.OnClickListener() {@Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    arrayList.remove(position);adapter.notifyDataSetChanged();
+//                    SharedPreferences sh=getApplicationContext().getSharedPreferences("safe", Context.MODE_PRIVATE);
+//                    HashSet<String> set=new HashSet<>(arrayList);sh.edit().putStringSet("notes",set).apply();
+//                    Toast.makeText(getApplicationContext(), "Deleted this Joke ,Now please Save again ...", Toast.LENGTH_SHORT).show();
+//                }
+//                }).setNegativeButton("No",null).show();
+//                return true;
+//            }
+//        });
         try {
             loadData();
         }catch (NullPointerException e){
             e.printStackTrace();
         }
     }
+    public void removeItem(List<String> list){
+        for (String i : list){
+            arrayList.remove(i);
+        }
+    }
+    public void saveItem(List<String> list){
+        for (String s : list) {
+            fileContent += s+"\n";
+        }
+        savedLogic(fileContent);
 
+    }
+    public void shareItem(List<String> list){
+        String select="";
+        for (String s : list) {
+            //  String s = arrayList.get(i);
+            select+=s+"\n";
+        }
+        sharedLogic(select);
+
+    }
+    public void sharedLogic(String s){
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Interesting Quote : " + s);
+        sendIntent.setType("text/plain");
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
+        Toast.makeText(getApplicationContext(), "Sharing Quote ...", Toast.LENGTH_SHORT).show();
+        finish();
+
+    }
+    public void savedLogic(String s){
+        if (!isExternalStorageAvailableForRW()) {
+            Toast.makeText(getApplicationContext(), "Sorry U don't have sdcard mounted on your device", Toast.LENGTH_SHORT).show();
+        } else {
+            if (!fileContent.equals("")) {
+                File file = new File(getExternalFilesDir(filePath), fileName);
+                FileOutputStream fileOutputStream = null;
+                try {
+                    fileOutputStream = new FileOutputStream(file);
+                    fileOutputStream.write(fileContent.getBytes());
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                Toast.makeText(JokesFavActivity.this, "Quote Saved to SD Card", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(JokesFavActivity.this, "Text Field can not be Empty", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
     private void loadData() {
         SharedPreferences sh = getSharedPreferences("safe", MODE_PRIVATE);
         Set<String> set = sh.getStringSet("notes", new HashSet<String>());
@@ -134,4 +174,55 @@ public class JokesFavActivity extends AppCompatActivity { String fileName="",fil
     }
     private boolean isExternalStorageAvailableForRW() { String extState= Environment.getExternalStorageState();
         if (extState.equals(Environment.MEDIA_MOUNTED)){ return true; } return false; }
+
+    AbsListView.MultiChoiceModeListener modeListener =new AbsListView.MultiChoiceModeListener() {
+        @Override
+        public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
+            if (userSelection.contains(arrayList.get(i))){
+                userSelection.remove(arrayList.get(i));
+            }else {
+                userSelection.add(arrayList.get(i));
+            }
+            actionMode.setTitle(userSelection.size() +" items selected...");
+        }
+
+        @Override
+        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+            MenuInflater menuInflater =actionMode.getMenuInflater();
+            menuInflater.inflate(R.menu.delete_save_share,menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.delete:
+                    removeItem(userSelection);
+                    adapter.notifyDataSetChanged();
+                    SharedPreferences sh = getApplicationContext().getSharedPreferences("safe", Context.MODE_PRIVATE);
+                    HashSet<String> set = new HashSet<>(arrayList);
+                    sh.edit().putStringSet("notes", set).apply();
+                    Toast.makeText(getApplicationContext(), "Deleted this Quote ...", Toast.LENGTH_SHORT).show();
+                    actionMode.finish();
+                    return true;
+                case R.id.share:
+                    shareItem(userSelection);
+                    return true;
+                case R.id.save:
+                    saveItem(userSelection);
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode actionMode) {
+            userSelection.clear();
+        }
+    };
 }
